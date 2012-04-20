83ac897292b4d75430829c1c7e1bf9caa1f2631a
Jared <jarcoal@gmail.com>
2012-04-19 17:44:57 -0700

    added label inclusion tag

--- bootstrap_inclusions/templatetags/bootstrap_misc.py
+++ bootstrap_inclusions/templatetags/bootstrap_misc.py
@@ -11,3 +11,14 @@ def icon(id, white=False):
 	:param white: If white should be used.  Pass in anything that resolves to True.
 	"""
 	return { 'id': id, 'white': white }
+
+
+@register.inclusion_tag('bootstrap_inclusions/misc/label.html')
+def label(text, kind=''):
+	"""
+	Generates a Bootstrap Label.
+	
+	:param text: Text to appear in the label.
+	:param kind: Kind of label: success, warning, important, info, inverse, or leave blank for default.
+	"""
+	return { 'text': text, 'kind': kind, }
